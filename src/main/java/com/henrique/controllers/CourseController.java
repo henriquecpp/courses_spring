060500436d252503ae21f00b40ed68ce95b40e31
henriquecpp <henriquedotcpp@gmail.com>
2023-09-04 19:39:21 -0300

    [feat] - Controller de cursos

---
+++ src/main/java/com/henrique/controllers/CourseController.java
@@ -0,0 +1,10 @@
+package com.henrique.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/courses")
+public class CourseController {
+
+}
