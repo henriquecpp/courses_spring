d62604891d7835f0b47dbcdab8e35c5cf5c8ea6c
henriquecpp <henriquedotcpp@gmail.com>
2023-09-04 23:16:37 -0300

    [chore] - refatorando o metodo de listagem

--- src/main/java/com/henrique/controllers/CourseController.java
+++ src/main/java/com/henrique/controllers/CourseController.java
@@ -1,10 +1,25 @@
 package com.henrique.controllers;
 
+import com.henrique.models.Course;
+import com.henrique.repository.CourseRepository;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/courses")
 public class CourseController {
 
+    private final CourseRepository courseRepository;
+
+    public CourseController(CourseRepository courseRepository) {
+        this.courseRepository = courseRepository;
+    }
+
+    @GetMapping
+    public List<Course> list() {
+        return courseRepository.findAll();
+    }
 }
