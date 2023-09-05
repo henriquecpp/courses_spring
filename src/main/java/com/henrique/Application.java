28267f59c4cc5398736699c3564fb1a5b5fc1019
henriquecpp <henriquedotcpp@gmail.com>
2023-09-04 23:17:25 -0300

    [chore] - criado metodo para iniciar o banco de dados "fake" (H2) com dados para fins de teste

--- src/main/java/com/henrique/Application.java
+++ src/main/java/com/henrique/Application.java
@@ -1,7 +1,11 @@
 package com.henrique;
 
+import com.henrique.models.Course;
+import com.henrique.repository.CourseRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
@@ -10,4 +14,16 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@Bean
+	CommandLineRunner initDatabase(CourseRepository courseRepository) {
+		return  args -> {
+			courseRepository.deleteAll();
+
+			Course c = new Course();
+			c.setName("Angular com Spring");
+			c.setCategory("front-end");
+
+			courseRepository.save(c);
+		};
+	}
 }
